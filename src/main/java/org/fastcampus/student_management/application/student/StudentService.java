5ec66174d48f69d88b9099b1ef4eb5c262b8acb9
doyeol <oktui2002@naver.com>
2025-03-01 02:17:08 +0900

    Refactor activateStudent method in StudentService to follow DDD principles

--- src/main/java/org/fastcampus/student_management/application/student/StudentService.java
+++ src/main/java/org/fastcampus/student_management/application/student/StudentService.java
@@ -25,16 +25,12 @@ public class StudentService {
   public void activateStudent(String name) {
     // TODO: 과제 구현 부분
     Student student = getStudent(name);
-    if(student.isActivate())
-      throw new IllegalArgumentException();
-    deactivateStudent(name);
+    student.activate();
   }
 
   public void deactivateStudent(String name) {
     // TODO: 과제 구현 부분
     Student student = getStudent(name);
-    if(!student.isActivate())
-      throw new IllegalArgumentException();
-    activateStudent(name);
+    student.deactivate();
   }
 }
